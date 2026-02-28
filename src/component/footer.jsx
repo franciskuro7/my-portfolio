60cc8e1b3d69d3a4c168398daa388f193302913e
Francis Kuro <franciskuro7@gmail.com>
2026-02-28 20:51:52 +0100

    Updating footer icon and LinkedIn link

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,7 +1,7 @@
 import * as S from '../styles/footer-styles';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { BsMailbox } from 'react-icons/bs';
-import { AiOutlineTwitter } from 'react-icons/ai';
+import { FaFacebookF } from 'react-icons/fa6';
 import { HiEnvelope } from 'react-icons/hi2';
 
 
@@ -16,8 +16,8 @@ const Footer = () => {
 
         <S.SocialIcons>
           <a href="https://github.com/franciskuro7/"><FaGithub size={18} /></a>
-          <a href="#"><FaLinkedin size={18} /></a>
-          <a href="#"><AiOutlineTwitter size={18} /></a>
+          <a href="https://www.linkedin.com/in/fineface-francis-kuroghoekigha-762158279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedin size={18} /></a>
+          <a FaFacebookF size={18} /></a>
           <a href="mailto:franciskuro7@gmail.com"><HiEnvelope size={18} /></a>
         </S.SocialIcons>
       </S.FooterTop>
